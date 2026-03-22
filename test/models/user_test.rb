d0dad26a65ae6933bdaab53b2f1b392ea0f3a31c
Robbity <robbielaughlen@gmail.com>
2026-03-22 14:45:31 +0000

    test: add user and rating model coverage

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,18 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(email: " DOWNCASED@EXAMPLE.COM ")
     assert_equal("downcased@example.com", user.email)
   end
+
+  test "requires username" do
+    user = User.new(email: "new@example.com", password: "password")
+
+    refute user.valid?
+    assert_includes user.errors[:username], "can't be blank"
+  end
+
+  test "requires unique username" do
+    user = User.new(username: users(:one).username, email: "unique@example.com", password: "password")
+
+    refute user.valid?
+    assert_includes user.errors[:username], "has already been taken"
+  end
 end
