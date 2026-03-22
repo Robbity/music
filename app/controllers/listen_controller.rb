7d929e7274729c6ac0c2c3a2f8175ebfd5fe22ee
Robbity <robbielaughlen@gmail.com>
2026-03-22 23:44:59 +0000

    fix: keep daily song until rated

--- app/controllers/listen_controller.rb
+++ app/controllers/listen_controller.rb
@@ -3,7 +3,6 @@ class ListenController < ApplicationController
     @song = user_signed_in? ? random_song_for_user : song_of_the_day
     if user_signed_in? && @song.present?
       @user_rating = current_user.ratings.find_by(song: @song)
-      current_user.listens.find_or_create_by(song: @song)
     end
     if user_signed_in? && params[:prompt_rating_id].present?
       @prompt_rating = current_user.ratings.find_by(id: params[:prompt_rating_id])
@@ -32,11 +31,9 @@ class ListenController < ApplicationController
         .includes(:user)
         .where.not(user_id: current_user.id)
 
-      listened_ids = current_user.listens.select(:song_id)
       rated_ids = current_user.ratings.select(:song_id)
 
       base
-        .where.not(id: listened_ids)
         .where.not(id: rated_ids)
         .order(Arel.sql("RANDOM()"))
         .first
