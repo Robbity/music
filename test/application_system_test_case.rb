83b81a425134352b2e38897cf3c8bd2d8747def8
Robbity <robbielaughlen@gmail.com>
2026-03-24 11:42:13 +0000

    chore: remove system test dependencies

--- test/application_system_test_case.rb
+++
@@ -1,6 +0,0 @@
-require "test_helper"
-require "capybara/cuprite"
-
-class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :cuprite, options: { window_size: [ 1280, 800 ], timeout: 10 }
-end
