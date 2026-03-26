2c8ed0b7a93ecb0f0c17a941bfc831517ddc14e5
Robbity <robbielaughlen@gmail.com>
2026-03-26 15:24:01 +0000

    daily songs schema... i feel this was already there

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_22_220700) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_22_223500) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,8 +20,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_22_220700) do
     t.string "name", null: false
     t.bigint "record_id", null: false
     t.string "record_type", null: false
-    t.index [ "blob_id" ], name: "index_active_storage_attachments_on_blob_id"
-    t.index [ "record_type", "record_id", "name", "blob_id" ], name: "index_active_storage_attachments_uniqueness", unique: true
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
@@ -33,13 +33,24 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_22_220700) do
     t.string "key", null: false
     t.text "metadata"
     t.string "service_name", null: false
-    t.index [ "key" ], name: "index_active_storage_blobs_on_key", unique: true
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
-    t.index [ "blob_id", "variation_digest" ], name: "index_active_storage_variant_records_uniqueness", unique: true
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "daily_songs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.date "date", null: false
+    t.bigint "song_id", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["song_id"], name: "index_daily_songs_on_song_id"
+    t.index ["user_id", "date"], name: "index_daily_songs_on_user_id_and_date", unique: true
+    t.index ["user_id"], name: "index_daily_songs_on_user_id"
   end
 
   create_table "ratings", force: :cascade do |t|
@@ -49,8 +60,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_22_220700) do
     t.integer "stars"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.index [ "song_id" ], name: "index_ratings_on_song_id"
-    t.index [ "user_id" ], name: "index_ratings_on_user_id"
+    t.index ["song_id"], name: "index_ratings_on_song_id"
+    t.index ["user_id"], name: "index_ratings_on_user_id"
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -59,7 +70,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_22_220700) do
     t.datetime "updated_at", null: false
     t.string "user_agent"
     t.bigint "user_id", null: false
-    t.index [ "user_id" ], name: "index_sessions_on_user_id"
+    t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "songs", force: :cascade do |t|
@@ -69,7 +80,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_22_220700) do
     t.string "title"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.index [ "user_id" ], name: "index_songs_on_user_id"
+    t.index ["user_id"], name: "index_songs_on_user_id"
   end
 
   create_table "test_uploads", force: :cascade do |t|
@@ -84,11 +95,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_22_220700) do
     t.string "encrypted_password", default: "", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.index [ "email" ], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "daily_songs", "songs"
+  add_foreign_key "daily_songs", "users"
   add_foreign_key "ratings", "songs"
   add_foreign_key "ratings", "users"
   add_foreign_key "sessions", "users"
